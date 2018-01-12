c7767681ec399cd4eb29807d5494991b54dd86f6
Nhi Chung <nhibchung@gmail.com>
2018-01-12 15:19:30 -0800

    Updating NLP licence

--- 2016 Election Webmap/NLP.java
+++ 2016 Election Webmap/NLP.java
@@ -1,8 +1,8 @@
-package edu.geog.ucsb.twitterproject;
+/* Stanford CoreNLP is licensed under the GNU General Public License (v3 or later; in general Stanford NLP code is GPL v2+, but CoreNLP uses several Apache-licensed libraries, and so the composite is v3+). Note that the license is the full GPL, which allows many free uses, but not its use in proprietary software which is distributed to others. For distributors of proprietary software, CoreNLP is also available from Stanford under a commercial licensing You can contact us at java-nlp-support@lists.stanford.edu. If you don’t need a commercial license, but would like to support maintenance of these tools, we welcome gift funding: use this form and write “Stanford NLP Group open source software” in the Special Instructions. */
 
+package edu.geog.ucsb.twitterproject;
 import java.util.List;
 import java.util.Properties;
-
 import edu.stanford.nlp.ling.CoreAnnotations;
 import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
 import edu.stanford.nlp.neural.rnn.RNNCoreAnnotations;
