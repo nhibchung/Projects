e58b1a4cb592c0c2cf126da738f1179d80611494
Nhi Chung <nhibchung@gmail.com>
2018-01-12 13:17:43 -0800

    Adding more scripts to Oculus Exploration project

---
+++ Oculus Rift Exploration Game Unity3D/Scripts/ShotCollision.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotCollision : MonoBehaviour
+{
+    public GameObject explosionPrefab;
+    public GameObject heartPrefab;
+
+    void Start()
+    {
+        // After arrow is instantiated(trigger pressed), destroy it after 5 seconds
+		Destroy(gameObject, 5.0f); 
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("shootable"))
+        {
+            //Instantiate explosion where arrow is
+			//Parent arrow's transform to target's transform (making arrow a child of target)
+			//Destroy ridgidbody & collider so arrow can stick to object
+            Instantiate(explosionPrefab, gameObject.transform.position, gameObject.transform.rotation);           
+            gameObject.transform.parent = collision.gameObject.transform;
+            Destroy(gameObject.GetComponent<Rigidbody>());
+            Destroy(gameObject.GetComponent<Collider>());
+            Destroy(collision.gameObject, 0.8f);
+        } 
+		else if (collision.gameObject.CompareTag("arrowTarget"))
+        {
+            //Parent arrow's transform to target's transform (making arrow a child of target)
+			//Destroy ridgidbody & collider so arrow can stick to object
+            gameObject.transform.parent = collision.gameObject.transform;
+            Destroy(gameObject.GetComponent<Rigidbody>());
+            Destroy(gameObject.GetComponent<Collider>());
+        } 
+        else if (collision.gameObject.CompareTag("animal"))
+        {
+            //Instantiate explosion where arrow is
+            Instantiate(heartPrefab, gameObject.transform.position, gameObject.transform.rotation);
+            Destroy(gameObject);
+        }
+    }
+}
+
---
+++ Oculus Rift Exploration Game Unity3D/Scripts/ThrowCollision.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowCollision : MonoBehaviour {
+
+    public GameObject heartPrefab;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("animal"))
+        {
+            Instantiate(heartPrefab, gameObject.transform.position, gameObject.transform.rotation);
+            Destroy(gameObject);
+        }
+    }
+}
